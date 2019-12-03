c0de9628ed076064362d63ec1b7b780e6dd2c574
mitchellmdoundle <57095970+mitchellmdoundle@users.noreply.github.com>
2019-12-03 12:55:45 +0000

    gsdfhg

--- addtoorder.php
+++ addtoorder.php
@@ -1,11 +1,14 @@
 <?php
-header("Refresh:2; url= order.php");
+header("Refresh:5; url= order.php");
 print_r($_POST);
 try{
     include_once("connection.php");
     array_map("htmlspecialchars", $_POST);
-    $stmt = $conn->prepare("INSERT INTO Basket(OrderID,SandwichID,SnackID,Sausage,DrinkID)VALUES (null,null,null,null,:sausage,null)");
-            $stmt->bindParam(':sausage', $_POST["sausage"]);
+    $stmt = $conn->prepare("INSERT INTO basket(OrderID,SandwichID,SnackID,Sausage,DrinkID)VALUES (null,:sandwich,:snack,:sausage,:drink)");
+    $stmt->bindParam(':sandwich', $_POST["sandwich"]);
+    $stmt->bindParam(':sausage', $_POST["sausage"]);
+    $stmt->bindParam(':snack', $_POST["snack"]);
+    $stmt->bindParam(':drink', $_POST["drink"]);
             $stmt->execute();
             }
 catch(PDOException $e)
